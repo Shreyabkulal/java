010e97caf1a947a300c60178879df0bae1ea87e8
Shreya B <shreya.Kulal@skillhousehost.onmicrosoft.com>
2024-06-20 13:51:54 +0530

    Java File

---
+++ TableFan.java
@@ -0,0 +1,12 @@
+class TableFan{
+
+   public static void main(String[] fan){
+   
+    System.out.println("Our small fan for desks or emergency fan are thoughtfully designed to fit snugly in any space. ");
+    System.out.println("Whether you place it on your office table or kitchen counter, the compact size of mini fan or table fan small size portable ensures a seamless fit.");   
+    System.out.println("Also Known as mini fan,usb fan portable high speed , small table fan ,Summer fan or charging fan.");   
+     	
+	
+   }
+
+}
