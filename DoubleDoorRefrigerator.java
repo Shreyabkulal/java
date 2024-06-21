8c6cf65f050e19b5b658e067d8f4bebb1cbf1101
Shreya B <shreyabkulal@gmail.com>
2024-06-21 14:32:42 +0530

    SOP

---
+++ DoubleDoorRefrigerator.java
@@ -0,0 +1,12 @@
+class DoubleDoorRefrigerator{
+
+   public static void main(String[] refrigerator){
+   
+    System.out.println("Frost Free Double Refrigerator Less than 300 L: Auto defrost function to prevent ice-build up");
+    System.out.println("Brand specific features: Multi-Air Flow cooling system- maintains optimal humidity and temperature levels so your food stays fresher for longer; Anti-Bacteria Gasket: prevents the entry and build-up of fungi and bacteria inside the refrigerator; Works Without Stabilizer (100~310V); Chiller- Shelf RU; Handle Type- Royale");   
+    System.out.println("Special Features: Offers you more space to suit your needs; Smart diagnosis- is a fast and easy way to troubleshoot issues; Smart Connect- helps connect your refrigerator to home inverter; Deodorizer");   
+    System.out.println("Smart inverter compressor: energy efficient; less noise & more durable");   	
+	
+   }
+
+}
